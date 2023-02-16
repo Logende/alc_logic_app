31f5cbb56178100c08e452fef87f420bd3427a1f
Felix Neubauer <felix@neuby.de>
2023-02-16 23:17:04 +0100

    updated satisfiability_algorithm

--- coursework/task_generation/satisfiability_algorithm.go
+++ coursework/task_generation/satisfiability_algorithm.go
@@ -3,23 +3,85 @@ package main
 func isSatisfiable(concept interface{}) bool {
 	// convert to NNF
 	concept = BNF2NNF(concept)
-	tableaus := Tableau{
-		elementConcepts: []interface{}{},
-		clash:           false,
-	}.computeSatisfiability(concept)
 
-	for _, tableau := range tableaus {
-		if !tableau.clash {
-			return true
-		}
+	tableaus := []Tableau{
+		Tableau{
+			elementConcepts: []interface{}{},
+			clash:           false,
+			relationships:   []Relationship{},
+			awaitingForEach: []QuantifierForEach{},
+		},
 	}
 
-	return false
+	return hasSolutionWithoutClash(tableaus)
 }
 
 type Tableau struct {
 	elementConcepts []interface{}
 	clash           bool
+	relationships   []Relationship
+	awaitingForEach []QuantifierForEach
+}
+
+type Relationship struct {
+	role                 Role
+	otherConceptTableaus []Tableau
+}
+
+func completeSatisfiabilityAlgorithm(tableaus []Tableau, concept interface{}) []Tableau {
+	tableaus = computeSatisfiability(tableaus, concept)
+
+	everythingDone := false
+
+	for !everythingDone {
+
+		everythingDone = true
+
+		for _, tableau := range tableaus {
+			if len(tableau.awaitingForEach) > 0 {
+				everythingDone = false
+				tableau.applyQuantifierForEachRule()
+			}
+		}
+
+		// new stuff might be added due to quantifier resolution
+		if !everythingDone {
+			tableaus = computeSatisfiability(tableaus, concept)
+		}
+
+	}
+
+	return tableaus
+}
+
+func (t Tableau) applyQuantifierForEachRule() {
+	for _, concept := range t.awaitingForEach {
+		for _, relationship := range t.relationships {
+			if relationship.role == concept.R {
+				relationship.otherConceptTableaus = completeSatisfiabilityAlgorithm(relationship.otherConceptTableaus, concept.C)
+			}
+		}
+	}
+
+}
+
+func hasSolutionWithoutClash(tableaus []Tableau) bool {
+	for _, tableau := range tableaus {
+		if !tableau.clash {
+			return true
+		}
+	}
+	return false
+}
+
+func computeSatisfiability(tableaus []Tableau, concept interface{}) []Tableau {
+	var result []Tableau
+
+	for _, tableau := range tableaus {
+		result = append(result, tableau.computeSatisfiability(concept)...)
+	}
+
+	return result
 }
 
 func (t Tableau) computeSatisfiability(concept interface{}) []Tableau {
@@ -67,14 +129,39 @@ func (t Tableau) computeSatisfiability(concept interface{}) []Tableau {
 			// should be copy because not using pointer
 			elementConcepts: t.elementConcepts,
 			clash:           t.clash,
+			relationships:   t.relationships,
 		}
 		tableauB := Tableau{
 			// should be copy because not using pointer
 			elementConcepts: t.elementConcepts,
 			clash:           t.clash,
+			relationships:   t.relationships,
 		}
 		tableaus = append(tableaus, tableauA.computeSatisfiability(concept.(OperatorIntersection).A)...)
 		tableaus = append(tableaus, tableauB.computeSatisfiability(concept.(OperatorIntersection).B)...)
+		break
+
+	case QuantifierExists:
+		otherConceptTableau := Tableau{
+			elementConcepts: []interface{}{},
+			clash:           false,
+			relationships:   []Relationship{},
+			awaitingForEach: []QuantifierForEach{},
+		}
+		relationship := Relationship{
+			role:                 concept.(QuantifierExists).R,
+			otherConceptTableaus: otherConceptTableau.computeSatisfiability(concept.(QuantifierExists).C),
+		}
+
+		t.relationships = append(t.relationships, relationship)
+		tableaus = []Tableau{t}
+		break
+
+	case QuantifierForEach:
+		// quantifierForEach we keep until the end. Only after having done everything else (including quantifierExists),
+		// do we go over quantifierForEach and check if they apply on all relationships
+		t.awaitingForEach = append(t.awaitingForEach, concept.(QuantifierForEach))
+		tableaus = []Tableau{t}
 		break
 
 	}
